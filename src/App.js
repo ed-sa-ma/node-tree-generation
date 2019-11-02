1b41fdfcd18ebe77a3a2033fb9068be725eac6b5
Eduardo Sanz Martin <>
2019-11-02 18:59:12 +0100

    implement App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import React from 'react';
 
+import List from "./components/List";
+import DataFormatter from "./components/DataFormatter";
+import useListOfNodes from "./hooks/useListOfNodes";
+
 function App() {
+  const list = useListOfNodes(500);
+
   return (
     <div className="App">
       <h1>Node tree generation</h1>
+      <DataFormatter data={list}>
+        {formattedData => (
+          <List list={formattedData} />
+        )}
+      </DataFormatter>
     </div>
   );
 }
