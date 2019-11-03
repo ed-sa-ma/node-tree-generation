64f031c543f56d912f1d5d92d570d8546a62820a
Eduardo Sanz Martin <>
2019-11-03 11:44:56 +0100

    add extra validation for node list generation

--- src/hooks/useListOfNodes.js
+++ src/hooks/useListOfNodes.js
@@ -2,11 +2,35 @@ import { useMemo } from "react";
 
 // This hook generates a random list of a given length when the component renders.
 const BOT_NAMES = ["Bruce", "Stan", "Mary", "Hugo", "Tom", "Pol", "Sam", "Mark", "Alice"];
-const pickRandomInt = (max) => Math.floor(Math.random()*max);
+const pickRandomInt = (max) => Math.floor(Math.random() * max);
+
+const isValidParentId = (id, item, list) => {
+  if (!item.parent) {
+    return true;
+  } else if (id === item.parent) {
+    return false;
+  } else {
+    const parentIndex = item.parent.match(/uuid(\d+)/)[1];
+    const parent = list[parentIndex];
+    
+    if (!parent) return true;
+
+    return isValidParentId(id, parent, list);
+  }
+};
+
+const assureParent = (item, list, length) => {
+  if (isValidParentId(item.id, item, list)) {
+    return [...list, item];
+  } else {
+    const newItem = { ...item, parent: `uuid${pickRandomInt(length)}` };
+    return assureParent(newItem, list, length);
+  }
+};
 
 const useListOfNodes = (length) => {
   const list = useMemo(() => {
-    const result = [];
+    let result = [];
 
     for (let i = 0; i < length; i++) {
       const item = {
@@ -16,8 +40,9 @@ const useListOfNodes = (length) => {
         ...(pickRandomInt(5) >= 1 && { parent: `uuid${pickRandomInt(length)}` }) 
       };
 
-      result.push(item);
+      result = assureParent(item, result, length);
     }
+
 
     return result;
   }, [length]);
